62fbf6ab7250dc0bcd99677c6e6a17cfdd979be0
BirupakhyaD <birupakhya.d@mpslimited.com>
2026-03-10 17:45:08 +0530

    Stage 1 final commit

--- DBGuard.Demo/AppDbContext.cs
+++ DBGuard.Demo/AppDbContext.cs
@@ -11,9 +11,9 @@ namespace DBGuard.Demo
     {
         public DbSet<User> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
         {
-            options.UseSqlServer("Data Source=10.66.13.24;Initial Catalog=journalitrack_uat;Persist Security Info=True;user id=skeltadbusr;password=skelta@bjd;Encrypt=False");
         }
     }
 }
--- DBGuard.Demo/Program.cs
+++ DBGuard.Demo/Program.cs
@@ -1,13 +1,53 @@
-﻿using DBGuard.Demo;
+﻿using DBGuard.Core.Diagnostics;
+using DBGuard.Demo;
+using DBGuard.EFCore.Extensions;
+using DBGuard.EFCore.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-var context = new AppDbContext();
+var connectionString = "server=192.168.1.17;user id=skeltadbusr;password=skelta@123;database=JournalItrack;TrustServerCertificate = True";
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddLogging(config =>
+        {
+            config.AddConsole();
+        });
+
+        services.AddDbGuard(connectionString);
+
+        services.AddDbContext<AppDbContext>((sp, options) =>
+        {
+            var interceptor = sp.GetRequiredService<DbGuardInterceptor>();
+
+            options.UseSqlServer(connectionString)
+                   .AddInterceptors(interceptor);
+        });
+    })
+    .Build();
+
+using var scope = host.Services.CreateScope();
+
+var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
 var user = new User
 {
-    FirstName = "ThisNameIsTooLongMoreThanColumnLengthAllowedInDatabase",
+    FirstName = "THIS_IS_A_VERY_LONG_NAME_EXCEEDING_COLUMN_LIMIT_123456",
     Email = "test@test.com"
 };
 
 context.Users.Add(user);
 
 await context.SaveChangesAsync();
+
+var metrics = scope.ServiceProvider.GetRequiredService<ValidationMetrics>();
+
+var failures = metrics.GetTopFailures();
+
+foreach (var f in failures)
+{
+    Console.WriteLine($"{f.Key} -> {f.Value}");
+}
